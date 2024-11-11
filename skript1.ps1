a495e4c26822e14aa6732a33c2564dc8ffa243e8
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-11 14:43:42 +0200

    Lisasin teise valemi

--- skript1.ps1
+++ skript1.ps1
@@ -3,12 +3,12 @@ $Array1 = @(1, 2, 3)
 $Array2 = @(4, 5, 6)
 
 # define empty result array
-$Array3 = @()
+$Array3 = New-Object -TypeName System.Collections.ArrayList
 
 # add new values to empty array
-$Array3 += $Array1[0] + $Array2[0]
-$Array3 += $Array1[1] + $Array2[1]
-$Array3 += $Array1[2] + $Array2[2]
+$Array3.Add($Array1[0] + $Array2[0]) | Out-Null
+$Array3.Add($Array1[1] + $Array2[1]) | Out-Null
+$Array3.Add($Array1[2] + $Array2[2]) | Out-Null
 
 # output result array
 $Array3
