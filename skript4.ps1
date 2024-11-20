260ea031b395aba49c09d8ef53c2eb2a8dc9274d
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-20 10:20:35 +0200

    Praks 4 - Suurem number on

---
+++ skript4.ps1
@@ -0,0 +1,19 @@
+﻿
+
+$number1 = Read-Host "Insert first number"
+$number2 = Read-Host "Insert second number"
+
+
+$biggerNumber = 0
+
+if($number1 -gt $number2){
+    $biggerNumber = $number1
+} elseif ($number2 -gt $number1){
+    $biggerNumber = $number2
+} else {
+    $biggerNumber = $number1
+    Write-Host "Numbers are equal, the biggest is: $biggerNumber"
+    exit 0
+}
+
+Write-Host "The higher number is: $biggerNumber"
