508f92e02c23d23ab29ead647d0aedff8caf0a5a
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-25 14:03:42 +0200

    Filesize ja name

---
+++ skript5.1.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem -path C:\Temp\Test
+
+Write-Host 
+
+$result = Get-ChildItem -path C:\Temp\Test | Where{$_.Name -like '*.csv'} | Select Length, Name
+
+
+Write-Host "Filename: $($result.Name)"
+Write-Host "Size in KB: $($result.Length/1kB)"
+Write-Host "Size in MB: $($result.Length/1MB)"
