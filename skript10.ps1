951152ecc430cf90da80ccbe0f26fe3d6b9acc0c
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-25 13:33:07 +0200

    do until while - count notepad

---
+++ skript10.ps1
@@ -0,0 +1,10 @@
+﻿$count = 0
+
+do {
+    Write-Host "Notepad is running" -ForegroundColor Green
+    $count++
+    Start-Sleep -Seconds 1
+    
+} until((Get-Process | where{$_.ProcessName -eq "notepad"}).Count -eq 0)
+
+Write-Host "$count"
