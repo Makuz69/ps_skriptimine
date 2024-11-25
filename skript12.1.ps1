6674e370a4a2e32ef858ceb4e500b950f7da6d0b
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-25 14:55:08 +0200

    Parameter countservice

---
+++ skript12.1.ps1
@@ -0,0 +1,22 @@
+﻿function countstart {
+    $total = (Get-Service | Where{$_.Status -eq 'Running'}).Count
+    Write-Host "Total services in running state = $($total)"
+}
+
+function countstop {
+    $total = (Get-Service | Where{$_.Status -eq 'Stopped'}).Count
+    Write-Host "Total services in stopped state = $($total)"
+}
+
+function countservice{
+    param(
+        [string]$status
+        )
+    $total = (Get-Service | Where{$_.Status -eq $status}).Count
+    Write-Host "Total services in $($status.ToLower()) state = $($total)"
+}
+
+
+
+countservice -status 'Running'
+countservice ('Stopped')
