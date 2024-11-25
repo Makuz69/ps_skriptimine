8fb68fcfbd51d90be3df89e852dd77dd00df57a2
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-11-25 14:49:42 +0200

    countstart ja countstop

---
+++ skript12.ps1
@@ -0,0 +1,12 @@
+﻿function countstart {
+    $total = (Get-Service | Where{$_.Status -eq 'Running'}).Count
+    Write-Host "Total services in running state = $($total)"
+}
+
+function countstop {
+    $total = (Get-Service | Where{$_.Status -eq 'Stopped'}).Count
+    Write-Host "Total services in stopped state = $($total)"
+}
+
+countstart
+countstop
